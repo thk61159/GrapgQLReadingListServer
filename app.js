cd54ae4bd2b3d03877331fa24f9134649807f904
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-04-25 14:01:35 +0800

    feat: simple data and query schema

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+
+const schema = require('./schema/schema')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('builded server')
+
+  res.send('<a href="/graphql">built server</a> <p>test nodemon</p>')
 })
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true, //using query gui
+	})
+)
 app.listen(process.env.PORT || 3000, () => {
   console.log('node server is running')
 })
