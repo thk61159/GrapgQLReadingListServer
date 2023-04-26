f661a3cc2faa1247f8c472fb2716364e1ff9895c
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-04-26 19:14:45 +0800

    feat: fix setting for deploy

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ RUN if [ "$NODE_ENV" = "development" ];\
         then npm install;\
         else npm install --only=production;\
         fi
-# RUN npm install
 COPY . ./ 
 ENV PORT 3000
 EXPOSE $PORT
--- config/config.js
+++ config/config.js
@@ -2,11 +2,10 @@ const MONGO_IP = process.env.MONGO_IP || 'mongo'
 const MONGO_PORT = process.env.MONGO_PORT || 27017
 const MONGO_USER = process.env.MONGO_USER
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD
-// const REDIS_URL = process.env.REDIS_URL || 'redis'
-// const REDIS_PORT = process.env.REDIS_PORT || 6379
-// const SESSION_SECRET = process.env.SESSION_SECRET
 
-const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/graphql?authSource=admin`
+
+const MONGO_URL = process.env
+	.MONGO_URL || `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/graphql?authSource=admin`
 
 module.exports = {
 	MONGO_IP,
